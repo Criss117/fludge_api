42b10a92432ffa5ad0b3cc010415135bf2a0e671
Cristian <95005685+Criss117@users.noreply.github.com>
2025-10-26 11:42:48 -0500

    feat: find one employee

--- src/employees/controllers/employees.controller.ts
+++ src/employees/controllers/employees.controller.ts
@@ -1,14 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateEmployeeUseCase } from '../usecases/create-employee.usecase';
 import { CreateEmployeeDto } from '../dtos/create-employee.dto';
 import { GetBusiness } from '@/auth/decorators/get-business.decorator';
 import { safeAction } from '@/shared/http/safe-action';
 import { HTTPResponse } from '@/shared/http/common.response';
 import { Permissions } from '@/auth/decorators/permissions.decorator';
+import { FindOneEmployeeUseCase } from '../usecases/find-one-employee.usecase';
 
 @Controller('businesses/:businessSlug/employees')
 export class EmployeesController {
-  constructor(private readonly createEmployeeUseCase: CreateEmployeeUseCase) {}
+  constructor(
+    private readonly createEmployeeUseCase: CreateEmployeeUseCase,
+    private readonly findOneEmployeeUseCase: FindOneEmployeeUseCase,
+  ) {}
 
   @Post()
   @Permissions('employees:create')
@@ -22,5 +26,18 @@ export class EmployeesController {
     );
 
     return HTTPResponse.created('Empleado creado correctamente');
+  }
+
+  @Get(':employeeId')
+  @Permissions('employees:read')
+  public async findOne(
+    @GetBusiness('id') businessId: string,
+    @Param('employeeId') employeeId: string,
+  ) {
+    const res = await safeAction(() =>
+      this.findOneEmployeeUseCase.execute(businessId, employeeId),
+    );
+
+    return HTTPResponse.ok('Empleado encontrado', res);
   }
 }
