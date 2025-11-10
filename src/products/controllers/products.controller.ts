c2494ffff0f1e0f5e6425664bf79aaed4ea88b44
Cristian <95005685+Criss117@users.noreply.github.com>
2025-11-10 13:21:40 -0500

    fix: return created product

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -33,14 +33,14 @@ export class ProductsController {
     @GetBusiness('id') businessId: string,
     @Body() values: CreateProductDto,
   ) {
-    await safeAction(
+    const response = await safeAction(
       () => this.createProductUsecase.execute(businessId, values),
       'No se pudo registrar el producto',
     );
 
     return HTTPResponse.created(
       'El producto se ha registrado correctamente',
-      null,
+      response,
     );
   }
 
